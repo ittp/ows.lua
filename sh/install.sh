9a0790b4fa91cde3b4294412728c09a26e4a8965
ittp <61617660+ittp@users.noreply.github.com>
2022-04-03 07:46:24 +0300

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-opkg update && opkg install openssh-server openssh-client openssh-sftp-client mount-utils kmod-ext4
+opkg update && opkg install openssh-server openssh-client openssh-sftp-client mount-utils kmod-fs-ext4 e2fsprogs block-mount fdisk
